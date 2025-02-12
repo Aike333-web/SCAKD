2257c21457dec917e6bd2654c4a4b60af6d455b5
XuJianhua <1332048386@xiaomi.com>
2025-02-12 19:57:39 +0800

    Update run_cifar_distill.sh

--- scripts/run_cifar_distill.sh
+++ scripts/run_cifar_distill.sh
@@ -20,8 +20,6 @@ python3 train_student.py --path-t ./save/models/resnet56_vanilla/ckpt_epoch_240.
         --batch_size 64 --learning_rate 0.05 \
         --save_model \
         --experiments_dir 'tea-res56-stu-res20/kd/global_T/your_experiment_name' \
-        --experiments_name 'fold-1'
+        --experiments_name 'fold-1' 
         
-# --path-t ./save/models/resnet32x4_vanilla/ckpt_epoch_240.pth --distill smd --model_s resnet8x4 -r 0.1 -a 0.9 -b 1 --kd_T 4 --batch_size 64 --learning_rate 0.05 --have_mlp 1 --mlp_name "global" --cosine_decay 1 --decay_max 0 --decay_min -1 --decay_loops 10 --save_model --experiments_dir 'tea-res32x4-stu-res8x4/kd/global_T/your_experiment_name' --experiments_name 'fold-1'
-
-# --path-t ./save/models/resnet56_vanilla/ckpt_epoch_240.pth --distill smd --model_s resnet20 -r 0.1 -a 0.9 -b 1 --kd_T 4 --batch_size 64 --learning_rate 0.05 --have_mlp 1 --mlp_name "global" --cosine_decay 1 --decay_max 0 --decay_min -1 --decay_loops 10 --save_model --experiments_dir 'tea-res56-stu-res20/kd/global_T/your_experiment_name' --experiments_name 'fold-1'
+python3 train_student.py --path-t /data/goujp/xjh/CTKD-main/save/models/cifar100/resnet56_vanilla/ckpt_epoch_240.pth --distill scakd --model_s resnet20 -r 0.1 -a 0.1 -b 1.0 -e 0.5 -f 1.0 --kd_T 4 --batch_size 64 --learning_rate 0.05 --save_model --experiments_dir 'TMM/tea-res56-stu-res20/' --experiments_name 'SCAKD'
