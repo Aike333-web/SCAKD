8b79d40f4ed82b39f1d126fe6243cd8c9aa8849b
XuJianhua <1332048386@xiaomi.com>
2025-02-13 12:49:51 +0800

    Update project

--- scripts/run_cifar_distill.sh
+++ scripts/run_cifar_distill.sh
@@ -14,12 +14,12 @@
 # python train_student.py --path-t ./save/models/resnet56_vanilla/ckpt_epoch_240.pth --distill dkd --model_s resnet20 -r 1 -a 0 -b 1 --dkd_alpha 1 --dkd_beta 2
 
 
-python3 train_student.py --path-t ./save/models/resnet56_vanilla/ckpt_epoch_240.pth \
+python3 train_student.py --path-t ./save/models/wrn_40_2_vanilla/ckpt_epoch_240.pth \
         --distill kd \
-        --model_s resnet20 -r 0.1 -a 0.9 -b 0 --kd_T 4 \
+        --model_s wrn_16_2 -r 0.1 -a 0.9 -b 0 --kd_T 4 \
         --batch_size 64 --learning_rate 0.05 \
         --save_model \
         --experiments_dir 'tea-res56-stu-res20/kd/global_T/your_experiment_name' \
         --experiments_name 'fold-1' 
         
-python3 train_student.py --path-t /data/goujp/xjh/CTKD-main/save/models/cifar100/resnet56_vanilla/ckpt_epoch_240.pth --distill scakd --model_s resnet20 -r 0.1 -a 0.1 -b 1.0 -e 0.5 -f 1.0 --kd_T 4 --batch_size 64 --learning_rate 0.05 --save_model --experiments_dir 'TMM/tea-res56-stu-res20/' --experiments_name 'SCAKD'
+python3 train_student.py --path-t /data/goujp/xjh/CTKD-main/save/models/cifar100/wrn_40_2_vanilla/ckpt_epoch_240.pth --distill scakd --model_s wrn_16_2 -r 0.1 -a 0.9 -b 1.0 -e 0.5 -f 0.5 --kd_T 4 --batch_size 64 --learning_rate 0.05 --save_model --experiments_dir 'tea-res56-stu-res20/' --experiments_name 'SCAKD'
